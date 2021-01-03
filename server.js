40249d63db9eb010d64254c88611741b8956617f
Yuichi <ozwald176@gmail.com>
2021-01-03 21:37:48 +0900

    update:210103

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.post('/submit', (req, res) =>{
 });
 
 // Listen to the App Engine-specified port, or 3000 otherwise
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
