0179325988a4145891664cfcd71a077780943820
Yuichi <ozwald176@gmail.com>
2021-01-05 13:04:32 +0900

    update:210105

--- public/js/result.js
+++ public/js/result.js
@@ -1,3 +1,10 @@
-var omikuji_result = document.getElementById("omikuji_result");
+var imgList = [
+    "picture/daikiti.jpg",
+    "picture/tyuukiti.jpg",
+    "picture/kiti.jpg",
+    "picture/syoukiti.jpg",
+    "picture/kyou.jpg"
+];
 
-omikuji_result.innerHTML = id;
+var omikuji_result = document.getElementById("omikuji_result");
+omikuji_result.innerHTML = "<img id='result_img' src='" + imgList[id] + "'>";
